b7ca18f50577e71b239b34ab84bbb9d02b1318ef
gael-src <src.gael@gmail.com>
2021-04-09 12:56:56 +0200

    header update

--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -1,9 +1,11 @@
 import "./main-header.css";
+import imgBart2 from "../../assets/images/dd-bart2.png";
 
 function MainHeader() {
   return (
     <div className="mainheader--wrapper">
-      <h2>Dylan Dog</h2>
+      <h2>L'INDAGATORE DELL'INCUBO</h2>
+      <img src={imgBart2} alt="Dylan Dog standing" />
     </div>
   );
 }
